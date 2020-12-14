8e9e908d772ffa39b380a86bf46906ff7011306e
Patrick Saitta <ptsaitta@gmail.com>
2020-12-14 15:22:40 -0500

    add table rows

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,4 +4,19 @@ var tableData = data;
 // YOUR CODE HERE!
 // looking at index.html, data should be placed in <tbody>
 
-const tbody = de.select("tbody");
+const tbody = d3.select("tbody");
+
+funct buildTable(data) {
+
+    //iteratively go through to add row to put each result's data in
+
+    data.forEach(tableRow) function() {
+        const row = tbody.append("tr")
+
+    }
+
+
+
+
+
+}
